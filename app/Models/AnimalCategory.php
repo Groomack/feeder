ddc69a16fdf75978d8cbd109c3355fa734f24552
Igore666ka <groomack@inbox.ru>
2026-01-07 02:53:07 +0500

    feat(db): create factory AnimalCategoryFactory and add it to DatabaseSeeder

--- app/Models/AnimalCategory.php
+++ app/Models/AnimalCategory.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AnimalCategory extends Model
 {
+    use HasFactory;
+    
     protected $table = 'animal_categories';
     protected $fillable = ['title'];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\AnimalCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(20)->create();
+        AnimalCategory::factory()->count(5)->create();
     }
 }
