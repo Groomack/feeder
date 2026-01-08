94a947b80dcf28d0fe0407d0fa65409853110481
Igore666ka <groomack@inbox.ru>
2026-01-08 05:30:49 +0500

    feat(auth): install breeze

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\AnimalCategoryController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('V1')->group(function () {
-    Route::apiResource('animal-category', AnimalCategoryController::class);
 });
