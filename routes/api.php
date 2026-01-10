b25f00c474ebba4834522b964a7c2b98547f4da7
Igore666ka <groomack@inbox.ru>
2026-01-10 06:50:22 +0500

    refactor(api): add animal-category routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AnimalCategoryController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('V1')->group(function() {
+    Route::apiResource('animal-category', AnimalCategoryController::class);
 });
 
 require __DIR__.'/auth.php';
